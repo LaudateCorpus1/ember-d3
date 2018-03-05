ac82b92039e5f592a4858a2658f256173fe1163b
Karim Beyrouti <karim.beyrouti@crowdstrike.com>
2018-03-05 15:02:57 +0000

    let ember use _findHost or use it's shim. This fixes ember addons not being able to find packages prefixed with d3

--- index.js
+++ index.js
@@ -151,24 +151,18 @@ module.exports = {
 function findTargetHost(addon, app) {
   let target = app
 
-  if (typeof addon.import === 'function') {
-    target = addon
-  } else {
-    // If the addon has the _findHost() method (in ember-cli >= 2.7.0), we'll just
-    // use that.
-    if (typeof addon._findHost === 'function') {
-      target = addon._findHost()
-    }
-
-    // Otherwise, we'll use this implementation borrowed from the _findHost()
-    // method in ember-cli.
-    // Keep iterating upward until we don't have a grandparent.
-    // Has to do this grandparent check because at some point we hit the project.
-    let current = addon
-    do {
-      target = current.app || app
-    } while (current.parent.parent && (current = current.parent))
+  if (typeof addon._findHost === 'function') {
+    target = addon._findHost()
   }
+
+  // Otherwise, we'll use this implementation borrowed from the _findHost()
+  // method in ember-cli.
+  // Keep iterating upward until we don't have a grandparent.
+  // Has to do this grandparent check because at some point we hit the project.
+  let current = addon
+  do {
+    target = current.app || app
+  } while (current.parent.parent && (current = current.parent))
 
   return target
 }
